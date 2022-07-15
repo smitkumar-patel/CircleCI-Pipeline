6932c354775f1aaaa30c569b9c139ecbc8da400c
DESKTOP-E6R1HCB\smitp <smitpatel945@gmail.com>
2022-07-15 14:53:21 -0400

    working code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import "dotenv/config";
+
 import { data } from "./mock-data/mock-data.js";
+
+const NON_SECRET_VAR = process.env.NON_SECRET_VAR;
 
 console.log(data);
 
-console.log("HMMMM");
+console.log(NON_SECRET_VAR);
+
+console.log("CHECK");
